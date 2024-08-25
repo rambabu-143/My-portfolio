ce7dcedb1307fa13bbd483771082ab98c6b4aa12
rambabu-143 <rambabuarabandi2001@gmail.com>
2024-08-25 17:33:55 +0530

    added the navigation to center and make it responsive

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Projects from "./projects";
 import Experience from "./experience";
 import Contact from "./contact";
@@ -22,64 +22,53 @@ const Tabs = () => {
   };
 
   return (
-    <div className="w-full  bg-gray-900 overflow-hidden">
-      <div className="flex space-x-4 border-b border-gray-800 mb-4">
+    <div className="w-full bg-gray-900 overflow-hidden">
+      <div className="flex justify-center py-8 items-center space-x-4 border-b border-gray-800 mb-4">
         <button
-          className={`py-2 px-4 ${activeTab === "projects" ? "border-b-2 border-red-600" : "text-gray-400"} hover:text-white`}
+          className={`py-2 px-4 text-lg  ${
+            activeTab === "projects"
+              ? "border-b-2 border-red-600"
+              : "text-gray-400"
+          } hover:text-white`}
           onClick={() => setActiveTab("projects")}
         >
           Projects
         </button>
         <button
-          className={`py-2 px-4 ${activeTab === "experience" ? "border-b-2 border-red-600" : "text-gray-400"} hover:text-white`}
+          className={`py-2 px-4 text-lg  ${
+            activeTab === "experience"
+              ? "border-b-2 border-red-600"
+              : "text-gray-400"
+          } hover:text-white`}
           onClick={() => setActiveTab("experience")}
         >
           Experience
         </button>
         <button
-          className={`py-2 px-4 ${activeTab === "contact" ? "border-b-2 border-red-600" : "text-gray-400"} hover:text-white`}
+          className={`py-2 px-4 text-lg  ${
+            activeTab === "contact"
+              ? "border-b-2 border-red-600"
+              : "text-gray-400"
+          } hover:text-white`}
           onClick={() => setActiveTab("contact")}
         >
           Contact
         </button>
       </div>
-      <div className="rounded-lg w-full">
-        <AnimatePresence>
-          {activeTab === "projects" && (
-            <motion.div
-              key="projects"
-              initial={{ opacity: 0, x: -100 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: 100 }}
-              transition={{ duration: 0.3 }}
-            >
-              {renderContent()}
-            </motion.div>
-          )}
-          {activeTab === "experience" && (
-            <motion.div
-              key="experience"
-              initial={{ opacity: 0, x: -100 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: 100 }}
-              transition={{ duration: 0.3 }}
-            >
-              {renderContent()}
-            </motion.div>
-          )}
-          {activeTab === "contact" && (
-            <motion.div
-              key="contact"
-              initial={{ opacity: 0, x: -100 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: 100 }}
-              transition={{ duration: 0.3 }}
-            >
-              {renderContent()}
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </div>
+      <motion.div
+        className="bg-gray-900 rounded-lg"
+        key={activeTab}
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 300,
+          damping: 30,
+          duration: 0.5,
+        }}
+      >
+        {renderContent()}
+      </motion.div>
     </div>
   );
 };
