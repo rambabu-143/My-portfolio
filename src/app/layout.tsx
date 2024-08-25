570043f2f12efdffb3e95459dff9e742745f95fe
rambabu-143 <rambabuarabandi2001@gmail.com>
2024-08-25 14:34:43 +0530

    Herosection is designed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local'
+ 
+const myFont = localFont({
+  src: '../../public/fonts/NothingFont5x7.woff2',
+  display: 'swap',
+})
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={myFont.className}>{children}</body>
     </html>
   );
 }
