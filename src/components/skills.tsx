6398aa868176ace3622b00d6a2bb71e961074386
rambabu-143 <rambabuarabandi2001@gmail.com>
2024-08-27 19:08:24 +0530

    added the responsive tab section

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -3,23 +3,23 @@ const Skills = () => {
     <div className="p-4 text-white">
       {/* Add your skills content here */}
       <h2 className="text-2xl w-full flex justify-center text-center font-bold mb-4">Skills</h2>
-      <ul className="list-none flex  justify-center items-center gap-8  flex-wrap p-5">
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">C</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">Java</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">Python</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">HTML</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">CSS</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">JavaScript</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">TypeScript</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">React.js</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">Tailwind CSS</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">Next.js</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">Framer Motion</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">Figma</li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">
+      <ul className="list-none  grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 text-center max-sm:grid-cols-2 max-md:grid-cols-3  place-items-center gap-8  p-5">
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">C</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">Java</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">Python</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">HTML</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">CSS</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">JavaScript</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">TypeScript</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">React.js</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">Tailwind CSS</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">Next.js</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">Framer Motion</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">Figma</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">
           Prototyping in Figma
         </li>
-        <li className="bg-gray-700 px-4 py-2 rounded-xl">Framer</li>
+        <li className="bg-gray-700 px-4 py-2 w-fit rounded-xl">Framer</li>
       </ul>
     </div>
   );
--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,11 +1,10 @@
-// tabs.js
 "use client";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Projects from "./projects";
 import Experience from "./experience";
 import Contact from "./contact";
-import Skills from "./skills"; // Import the Skills component
+import Skills from "./skills"; // Assuming you've added a Skills component
 
 const Tabs = () => {
   const [activeTab, setActiveTab] = useState("projects");
@@ -27,9 +26,9 @@ const Tabs = () => {
 
   return (
     <div className="w-full bg-black overflow-hidden">
-      <div className="flex justify-center py-8 items-center space-x-4 border-b border-gray-800 mb-4">
+      <div className="flex  justify-center items-center lg:text-xl py-8 px-4 sm:text-sm max-sm:text-sm md:text-sm max-md:text-sm ">
         <button
-          className={`py-2 px-4  ${
+          className={`py-2 px-4 w-full md:w-auto  ${
             activeTab === "projects"
               ? "border-b-2 border-red-600"
               : "text-gray-400"
@@ -39,7 +38,7 @@ const Tabs = () => {
           Projects
         </button>
         <button
-          className={`py-2 px-4  ${
+          className={`py-2 px-4 w-full md:w-auto  ${
             activeTab === "experience"
               ? "border-b-2 border-red-600"
               : "text-gray-400"
@@ -49,17 +48,7 @@ const Tabs = () => {
           Experience
         </button>
         <button
-          className={`py-2 px-4  ${
-            activeTab === "skills"
-              ? "border-b-2 border-red-600"
-              : "text-gray-400"
-          } hover:text-white`}
-          onClick={() => setActiveTab("skills")}
-        >
-          Skills
-        </button>
-        <button
-          className={`py-2 px-4  ${
+          className={`py-2 px-4 w-full md:w-auto  ${
             activeTab === "contact"
               ? "border-b-2 border-red-600"
               : "text-gray-400"
@@ -67,6 +56,16 @@ const Tabs = () => {
           onClick={() => setActiveTab("contact")}
         >
           Contact
+        </button>
+        <button
+          className={`py-2 px-4 w-full md:w-auto  ${
+            activeTab === "skills"
+              ? "border-b-2 border-red-600"
+              : "text-gray-400"
+          } hover:text-white`}
+          onClick={() => setActiveTab("skills")}
+        >
+          Skills
         </button>
       </div>
       <motion.div
